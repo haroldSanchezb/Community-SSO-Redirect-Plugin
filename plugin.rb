8692f1ca8427c663825ccec5b31d726ff86b3cd1
Harold Sanchez Balaguera <haroldsanchezb@Harolds-MacBook-Pro.local>
2015-06-10 10:22:33 -0500

    Cookie
    Added plugin settings.
    Added gitignore
    Added cookie JS function

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: Community SSO Redirect Plugin
+# about: Create a cross subdomain cookie for Community and Campus 
+# version: 0.1
+# authors: Harold Sanchez Balaguera
+
+register_asset 'javascripts/cookie.js'
