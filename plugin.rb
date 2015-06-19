fab90068a00db807df1de61898dc7c4f0674cc2e
Harold Sanchez Balaguera <haroldsanchezb@gmail.com>
2015-06-19 07:36:05 -0500

    New SSO Method
    Override actual sso method for new one, this include return path and create a cookie

--- plugin.rb
+++ plugin.rb
@@ -1,49 +1,28 @@
 # name: Community SSO Redirect Plugin
 # about: Create a cross subdomain cookie for Community and Campus 
-# version: 0.1
+# version: 0.5
 # authors: Harold Sanchez Balaguera
 
 after_initialize do
 	SessionController.class_eval do
-		skip_before_filter :check_xhr, only: ['sso', 'sso_login', 'become', 'sso_provider', 'sso_redirect']
 
-		def sso_redirect
-			user_cookie = cookies[:_t]
+		require_dependency 'single_sign_on'
 
-			unless user_cookie
-				nonce = DiscourseSingleSignOn.generate_url('/')
-				uri_array = Rack::Utils.parse_query(nonce)
+		skip_before_filter :check_xhr, only: ['sso', 'sso_login', 'become', 'sso_provider']
 
-				Rails.logger.info "uri_array #{uri_array}"
-				Rails.logger.info "SiteSetting.sso_url #{SiteSetting.sso_url}"
-
-				sso_param = '?sso'
-				unless SiteSetting.sso_url.blank?
-					sso_param = SiteSetting.sso_url + "?sso"
-				end
-
-				Rails.logger.info "sso_param #{sso_param}"
-
-				sso = CGI::escape(uri_array[sso_param])
-				sig = uri_array['sig']
-
+		def sso
+			if SiteSetting.enable_sso
+				url = DiscourseSingleSignOn.generate_url(params[:return_path] || path('/'))
 				return_url = Base64.encode64(CGI::escape(request.host))
 
-				if cookies[:destination_url]
-					return_url = Base64.encode64(CGI::escape(cookies[:destination_url]))
+				if cookies[:url_public]
+					return_url = Base64.encode64(CGI::escape(cookies[:url_public]))
 				end
 
-				sso_login_url = SiteSetting.sso_redirect_login
-				Rails.logger.info "sso_login_url #{sso_login_url}"
-
-				redirect_to "#{sso_login_url}?sso=#{sso}&sig=#{sig}&return=#{return_url}"
-			else 
-				redirect_to "/"
+				redirect_to "#{url}&return=#{return_url}"
+			else
+				render nothing: true, status: 404
 			end
 		end
-	end
-
-	Discourse::Application.routes.append do
-		get "session/sso_sign" => "session#sso_redirect"
 	end
 end
